dac029d627c9bda28bdbcdceb81e265ffd468b68
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-06-28 15:04:07 -0300

    Criando projeto

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <p>Bem vindo ao mundo do useReducer!</p>
+    </>
+  );
+}
+
+export default App;
